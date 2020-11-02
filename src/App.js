17b8345dd292684db74c5fa299637c8b85307b0e
DESKTOP-NUCAED7\ortiz <ortizalej199@gmail.com>
2020-11-02 20:57:40 -0300

    template user

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import ListUserComponent from "./user/ListUserComponent";
+import AddUserComponent from "./user/AddUserComponent";
+import EditUserComponent from "./user/EditUserComponent";
 import './App.css';
 import Admin from './pages/Administration'
 function App() {
   return (
     <div className="App">
-      <Admin/>
+      <Router>
+        <Switch>
+          <Route path="/" exact component={ListUserComponent} />
+          <Route path="/users" component={ListUserComponent} />
+          <Route path="/add-user" component={AddUserComponent} />
+          <Route path="/edit-user" component={EditUserComponent} />
+        </Switch>
+      </Router>
     </div>
   );
 }
